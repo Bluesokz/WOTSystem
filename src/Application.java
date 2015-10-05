9aa4b37b6f01280b93c8498859b71b7cfde61389
Bluesokz <chrispoole94@gmail.com>
2015-10-05 10:10:14 +0100

    IP update

--- src/Application.java
+++ src/Application.java
@@ -235,7 +235,7 @@ public class Application {
 	void initiate(){
 		try{
 			
-			String host = "jdbc:mysql://10.50.15.18:3306/wots";
+			String host = "jdbc:mysql://10.50.15.26:3306/wots";
 			String username = "root"; 
 			String password = "password";
 			conn = DriverManager.getConnection(host,username,password);
